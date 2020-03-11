56606c060e243d3b229bd0b03738141c318d49b3
ninhxtina <christinaninh07@yahoo.com>
2020-03-11 15:59:03 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    private static Integer[] data;
+
+    public static void main(String[] args) {
+        testMaxHeapLogN(data);
+
+        testMaxHeapN(data);
+    }
+
+    private static void testMaxHeapLogN(Integer[] data) {
+        //homework
+
+    }
+
+    private static void testMaxHeapN(Integer[] data) {
+        //homework
+
+    }
+}
